a1f0e18105f3d9b5dc5e7dfa805a7f81e10823c1
ilin001 <ilinyum01@gmail.com>
2020-11-25 16:49:57 +0100

    upload sources from Jacquot website

---
+++ squelPartie1/squelette1FRog/src/frog/Frog.java
@@ -0,0 +1,16 @@
+package frog;
+
+import gameCommons.Game;
+import gameCommons.IFrog;
+
+public class Frog implements IFrog {
+	
+	private Game game;
+
+
+}
+
+public Frog(Game game) {
+	this.game = game
+
+}
